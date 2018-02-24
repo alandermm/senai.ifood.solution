9b1ecbe3fde73dd5f68f8bc1b4a1a968e296c7c6
Alander Machado <alandermm@hotmail.com>
2018-02-23 22:03:49 -0300

    Terminar

--- senai.ifood.webapi/Startup.cs
+++ senai.ifood.webapi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using senai.ifood.repository.Context;
+using senai.ifood.repository.Repositories;
+using senai.ifood.domain.Contracts;
 
 namespace senai.ifood.webapi
 {
@@ -26,6 +28,12 @@ namespace senai.ifood.webapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<IFoodContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc().AddJsonOptions(options => {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +44,9 @@ namespace senai.ifood.webapi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+
+            
         }
     }
 }
